192010f8d35921b0104442259adf5595ec282be1
javier navas <j.navasdam@gmail.com>
2025-07-30 01:21:00 +0200

    Delete backend directory

--- backend/database/videojuegos.py
+++
@@ -1,6 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-MONGODB_URI="mongodb+srv://javier:javier@cluster0.t5cgbal.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = AsyncIOMotorClient(MONGODB_URI)
-db = client["videogame_db"]
-def get_database():
-    return db
